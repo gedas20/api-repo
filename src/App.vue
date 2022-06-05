6d00f891c725365396c41cdab084bae74ba5611d
gediminas bagdonas <gedas20@gmail.com>
2022-06-04 22:02:51 -0500

    add basic structure and load api data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CryptoExchangeRates
+    :cryptoCurrencies="cryptoExchangeRateData"
+  />
+ 
+  <FiatExchangeRates
+    :fiatCurrencies="fiatExchangeRateData"
+  />
+
+  <CommodityExchangeRates
+    :commodities="commodityExchangeRateData"
+  />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import CryptoExchangeRates from './components/CryptoExchangeRates.vue'
+  import FiatExchangeRates from './components/FiatExchangeRates.vue'
+  import CommodityExchangeRates from './components/CommodityExchangeRates.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      CryptoExchangeRates,
+      FiatExchangeRates,
+      CommodityExchangeRates
+    },
+    data() {
+      return {
+          fiatExchangeRateData: [],
+          cryptoExchangeRateData: [],
+          commodityExchangeRateData: [],
+          apiDataLoaded: false,
+          error: ''
+      }
+    },
+    methods: {
+      loadApiData() {
+        fetch('https://api.coingecko.com/api/v3/exchange_rates')
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+
+            } else {
+
+              this.error = 'Failed to fetch data.';
+            }
+          })
+          .then(data => {
+            if (data.rates) {
+    
+              const objRates = data.rates;
+
+              for (const property in objRates) {
+
+                let objCurrency = objRates[property];
+
+                if (objCurrency['type'] == 'crypto') {
+
+                   this.cryptoExchangeRateData.push(objCurrency);
+
+                } else if (objCurrency['type'] == 'fiat') {
+
+                  this.fiatExchangeRateData.push(objCurrency);
+
+                } else if (objCurrency['type'] == 'commodity') {
+
+                   this.commodityExchangeRateData.push(objCurrency);
+                }
+              }
+            } else {
+
+              this.error = 'Failed to fetch data.';
+            }
+          })
+          .catch(() => {
+
+              this.error = 'Failed to fetch data.';
+          });
+      },
+    },
+    mounted() {
+      this.loadApiData();
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/CryptoExchangeRates.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h2>Crypto Exchange Rates</h2>
+    <p>
+      
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CryptoExchangeRates',
+  props: {
+    cryptoCurrencies: {
+        type: Array,
+        required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
