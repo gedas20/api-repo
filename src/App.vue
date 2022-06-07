a413b047d1fc390d04be5e4d0d12d19b1f31ed28
gediminas bagdonas <gedas20@gmail.com>
2022-06-06 21:42:27 -0500

    add charts

--- src/App.vue
+++ src/App.vue
@@ -3,46 +3,72 @@
   <div class="box error" v-if="apiDataLoaded && error !== ''"> 
       {{ error }}
   </div>
-  <div v-else class="flex-container" v-cloak>
-    <div>
-      <ExchangeRates
-        title="Crypto"
-        :currencies="cryptoExchangeRateData"
-        v-on:showSymbolEvent="showSymbolName"
-      />
-    </div>
-    <div>
-      <ExchangeRates
-        title="Fiat"
-        :currencies="fiatExchangeRateData"
-        v-on:showSymbolEvent="showSymbolName"
-      />
-    </div>
-    <div>
-      <ExchangeRates
-        title="Commodity"
-        :currencies="commodityExchangeRateData"
-        v-on:showSymbolEvent="showSymbolName"
-      />
-    </div>
-    <div>
-      <ExchangeRates
-        title="Other"
-        :currencies="otherExchangeRateData"
-        v-on:showSymbolEvent="showSymbolName"
-      />
+  <div v-else v-cloak>
+    <div class="flex-container">
+      <div>
+        <ExchangeRates
+          title="Crypto"
+          :currencies="cryptoExchangeRateData"
+          v-on:showSymbolEvent="showSymbolName"
+        />
+      </div>
+      <div>
+        <ExchangeRates
+          title="Fiat"
+          :currencies="fiatExchangeRateData"
+          v-on:showSymbolEvent="showSymbolName"
+        />
+      </div>
+      <div>
+        <ExchangeRates
+          title="Commodity"
+          :currencies="commodityExchangeRateData"
+          v-on:showSymbolEvent="showSymbolName"
+        />
+      </div>
+      <div>
+        <ExchangeRates
+          title="Other"
+          :currencies="otherExchangeRateData"
+          v-on:showSymbolEvent="showSymbolName"
+        />
+      </div>
     </div>
   </div>
- 
+  <div>
+    <Bar
+        v-if="apiDataLoaded"
+        :chart-options="getBarChartOptions('Crypto')"
+        :chart-data="cryptoDataSet"
+        :height="100"
+    />
+    <Bar
+        v-if="apiDataLoaded"
+        :chart-options="getBarChartOptions('Fiat')"
+        :chart-data="fiatDataSet"
+        :height="100"
+    />
+    <Bar
+        v-if="apiDataLoaded"
+        :chart-options="getBarChartOptions('Commodity')"
+        :chart-data="commodityDataSet"
+        :height="100"
+      />
+  </div>
 </template>
 
 <script>
   import ExchangeRates from './components/ExchangeRates.vue'
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+  ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
 
   export default {
     name: 'App',
     components: {
-      ExchangeRates
+      ExchangeRates,
+      Bar
     },
     data() {
       return {
@@ -54,7 +80,73 @@
           error: ''
       }
     },
+    computed: {
+      cryptoDataSet() {
+        const labels = [];
+        const data = [];
+        
+        for (const index in this.cryptoExchangeRateData) {
+          const objComodity = this.cryptoExchangeRateData[index];
+          if (!["sats", "μBTC", "XLM", "XRP", "EOS", "LINK", "DOT"].includes(objComodity.unit)) {
+            labels.push(objComodity.unit);
+            data.push(objComodity.value);
+          }
+        }
+        
+        return this.getBarChartDataSet(labels, data);
+      },
+      fiatDataSet() {
+        const labels = [];
+        const data = [];
+        
+        for (const index in this.fiatExchangeRateData) {
+          const objComodity = this.fiatExchangeRateData[index];
+          if (!["₫", "Rp", "K", "₩", "CLP$"].includes(objComodity.unit)) {
+            labels.push(objComodity.unit);
+            data.push(objComodity.value);
+          }
+        }
+        
+        return this.getBarChartDataSet(labels, data);
+      },
+      commodityDataSet() {
+        const labels = [];
+        const data = [];
+        
+        for (const index in this.commodityExchangeRateData) {
+          const objComodity = this.commodityExchangeRateData[index];
+          labels.push(objComodity.unit);
+          data.push(objComodity.value);
+        }
+        
+        return this.getBarChartDataSet(labels, data);
+      }
+    },
     methods: {
+      getBarChartDataSet(labels, data) {
+        return {
+            labels: labels,
+            datasets: [
+                {
+                    backgroundColor: '#337ab7',
+                    data: data
+                }
+            ]
+        }
+      },
+      getBarChartOptions(title) {
+        return {
+          plugins: {
+            legend: {
+              display: false,
+            },
+            title: {
+              display: true,
+              text: title
+            }
+          }
+        }
+      },
       showSymbolName(symbol) {
         alert(symbol);
       },
